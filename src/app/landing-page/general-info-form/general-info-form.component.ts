3cdfc60551cc94a59056aaca879956b5370468b4
Sergej <sergej1995@gmail.com>
2018-10-12 10:06:25 +0200

    Adapt general-info-form component

---
+++ src/app/landing-page/general-info-form/general-info-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'landing-page-general-info-form',
+  templateUrl: './general-info-form.component.html',
+  styleUrls: ['./general-info-form.component.css']
+})
+export class GeneralInfoFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
