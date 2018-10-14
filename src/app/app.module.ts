883df5e6fdaff5c90f374671839d8fbed6b9bf2e
Sergej <sergej1995@gmail.com>
2018-10-14 19:13:26 +0200

    Add @ngstack/translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
+import { TranslateModule, TranslateService } from '@ngstack/translate';
 
 import { AppRoutingModule } from './app-routing.module';
 import { LandingPageModule } from './landing-page/landing-page.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+
+// needed to load translation before application starts
+export function setupTranslateService(service: TranslateService) {
+  return () => service.load();
+}
+
 
 @NgModule({
   declarations: [
@@ -15,10 +22,21 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    TranslateModule.forRoot({
+      activeLang: 'de'
+    }),
     LandingPageModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    // needed to load translation before application starts
+    {
+      provide: APP_INITIALIZER,
+      useFactory: setupTranslateService,
+      deps: [TranslateService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
