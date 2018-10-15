c46f5c02e2e9ddd22b645a742f7abf5a5d1fc3ab
Sergej <sergej1995@gmail.com>
2018-10-15 16:21:00 +0200

    Add validators

--- src/app/landing-page/general-info-form/general-info-form.component.ts
+++ src/app/landing-page/general-info-form/general-info-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'landing-page-general-info-form',
@@ -11,19 +11,35 @@ export class GeneralInfoFormComponent implements OnInit {
   generalInfoForm: FormGroup;
 
   constructor(formBuilder: FormBuilder) {
-    this.generalInfoForm = formBuilder.group({  
-      'age': [''],
-      'gender': ['Male'],
-      'email': [''],
-      'agreedToTerms': ['false']  
-    });  
-   }
+    this.generalInfoForm = formBuilder.group({
+      'age': ['', Validators.compose([
+        Validators.required, this.ageValidator])],
+      'gender': ['Male', Validators.required],
+      'email': ['', Validators.email],
+      /* pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$') */
+      'agreedToTerms': ['false', Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(form: any): void {  
-    console.log('you submitted value:', form);  
+  emailValidator(control: FormControl): { [s: string]: boolean } {
+    if (!control.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/)) {
+      return { invalidEmail: true };
+    }
+  }
+
+  ageValidator(control: FormControl): { [s: string]: boolean } {
+    if (control.value < 13) {
+      return { invalidAge: true };
+    }
+  }
+
+  onSubmit(form: FormGroup): void {
+
+    console.log('you submitted value:', form);
+
   }
 
 }
