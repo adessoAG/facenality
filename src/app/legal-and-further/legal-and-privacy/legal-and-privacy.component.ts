a48bdafd73010ca2c48523d3e7016d47c86610df
Sergej Grilborzer <sergej1995@gmail.com>
2018-11-11 16:47:53 +0100

    Alpa-Launch preparation
    Add logo & icons, adjust minor points in different components

--- src/app/legal-and-further/legal-and-privacy/legal-and-privacy.component.ts
+++ src/app/legal-and-further/legal-and-privacy/legal-and-privacy.component.ts
@@ -7,16 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LegalAndPrivacyComponent implements OnInit {
 
-  showFirstChapter = false;
-  showSecondChapter = false;
-  showThirdChapter = false;
-  showFourthChapter = false;
-  showFifthChapter = false;
-  showSixthChapter = false;
-  showConsentForm = false;
+  chaptersVisible: { i: number, isVisible: boolean }[] =
+    [
+      { i: 0, isVisible: false },
+      { i: 1, isVisible: false },
+      { i: 2, isVisible: false },
+      { i: 3, isVisible: false },
+      { i: 4, isVisible: false },
+      { i: 0, isVisible: false },
+      { i: 5, isVisible: false }
+    ]
 
   constructor() { }
 
   ngOnInit() { }
 
+  toggleChapterVisibility(i: number) {
+    this.chaptersVisible[i].isVisible = !this.chaptersVisible[i].isVisible; 
+  }
 }
